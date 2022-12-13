0b034a41a5fd35fc7ee082c1c6367b6dce5e1a8d
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 20:12:02 -0700

    added answer key index file for convenience and commented out old working code

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { fetchMyIP } = require("./iss");
-const ipURL = "https://api.ipify.org/?format=json";
+const { fetchMyIP } = require('./iss');
 
-fetchMyIP(ipURL, (error, data) => {
+fetchMyIP((error, ip) => {
   if (error) {
-    console.log(error);
-  } else {
-    console.log(data)
+    console.log("It didn't work!" , error);
+    return;
   }
-})
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+// const ipURL = "https://api.ipify.org/?format=json";
+
+// fetchMyIP(ipURL, (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data)
+//   }
+// })
