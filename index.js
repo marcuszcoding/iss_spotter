eb3d4bf6748b163b0085634ec898ab093ea3bac8
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 21:29:38 -0700

    implemented fetch coords function test

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+fetchCoordsByIP('124.151.221.154', (error, coordinates) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("Sorry, didnt work!", error)
     return;
-  }
+  } 
 
-  console.log('It worked! Returned IP:' , ip);
-});
+  console.log('It worked! Returned coordinates:', coordinates)
+})
+
 
 // const ipURL = "https://api.ipify.org/?format=json";
 
