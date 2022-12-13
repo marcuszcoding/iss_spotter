c03039901eec96fb6415a378ad1fb65d75964577
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 21:52:36 -0700

    initial set up for fetchISS, just starting with a URL variable and copy pasting my previous code as a boiler plate for the function

--- iss.js
+++ iss.js
@@ -1,11 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
  const request = require('request');
 
  const fetchMyIP = function(callback) {
@@ -40,7 +32,28 @@ const fetchCoordsByIP = function(ip, callback) {
   })
 }
 
-module.exports = { fetchCoordsByIP };
+//Going to use my original format of using a URL vairable to make it easier
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const URL = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  
+  request(URL, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const parsedBody = JSON.parse(body);
+    // check if "success" is true or not
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    const { latitude, longitude } = parsedBody
+
+    callback(null, {latitude, longitude})
+  })
+};
+
+module.exports = { fetchISSFlyOverTimes };
 //  const request = require('request');
 
 //  const fetchMyIP = function(ipURL, callback) { 
