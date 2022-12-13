015b1d2aa4342cc7a04cd5624c26437bfbba9cff
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 23:24:27 -0700

    added index test for new nextISSTimesForMyLocation function and added a variable which printsPassTimes with the proper syntax

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
 
-const fakeCoords = {latitude: '68.14497', longitude: '13.53922'}
+// const fakeCoords = {latitude: '68.14497', longitude: '13.53922'}
 
 // // fetchMyIP((error, ip) => {
 // //   if (error) {
@@ -18,22 +18,39 @@ const fakeCoords = {latitude: '68.14497', longitude: '13.53922'}
 //   if (error) {
 //     console.log("Sorry, didnt work!", error)
 //     return;
-//   } 
+//   }
 
 //   console.log('It worked! Returned coordinates:', coordinates)
 // })
 
 
-fetchISSFlyOverTimes(fakeCoords, (error, passes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchISSFlyOverTimes(fakeCoords, (error, passes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Passes:' , passes);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('It worked! Returned Passes:' , passes);
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
-
-
 
 
 // Old code
