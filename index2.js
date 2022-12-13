33ed0dba8299e9aff2f00b7bd6d64be39e5f7cbb
marcuszcoding <marcuszcoding@gmail.com>
2022-12-13 11:31:31 -0700

    working promise callback chain but using all 3 functions, initial exercise

---
+++ index2.js
@@ -0,0 +1,7 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+  
