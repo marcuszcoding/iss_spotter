24ee9e87b29fd1e929d3e447e9e149fd0231ab91
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 20:11:38 -0700

    updated code to answer key for convenience and commented out working old code for now

--- iss.js
+++ iss.js
@@ -6,18 +6,30 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+ const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
 
- const request = require('request');
-
- const fetchMyIP = function(ipURL, callback) { 
-  request(ipURL, (error, response, body) => {
-    if (error) {
-      console.log("There was an error getting the IP")
-      return callback(error, null)
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
     }
-    const data = JSON.parse(body);
-    return callback(null, data)
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
-}
+};
 
 module.exports = { fetchMyIP };
+//  const request = require('request');
+
+//  const fetchMyIP = function(ipURL, callback) { 
+//   request(ipURL, (error, response, body) => {
+//     if (error) {
+//       console.log("There was an error getting the IP")
+//       return callback(error, null)
+//     }
+//     const data = JSON.parse(body);
+//     return callback(null, data)
+//   });
+// }
